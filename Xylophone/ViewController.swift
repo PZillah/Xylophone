edc0e5b1e21aee7cfecc0ca7702559d75421aa70
priscilla <priscillakung@gmail.com>
2023-05-06 17:56:32 -0700

    make button half opaque when pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,9 +19,15 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
-        
-        print(sender.currentTitle)
-        print(sender.titleLabel?.text)
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        print("Start")
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+            print("End")
+        }
     }
     //1. grabbing the sound file from resources
     //2. then we tell audio player which file to play
@@ -30,7 +36,6 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
 
